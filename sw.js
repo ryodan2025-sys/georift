9578526fe8571061f208a6efc60923b7f4544e99
ryodan2025-sys <ryodan2025@gmail.com>
2026-04-09 09:21:16 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const VERSION = 'v3';
-const CACHE = 'georift-' + VERSION;
-const FILES = ['./', './index.html',
-  './elements.js'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
-self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).then(res => { const clone = res.clone(); caches.open(CACHE).then(c => c.put(e.request, clone)); return res; }).catch(() => caches.match(e.request))); });
-self.addEventListener('message', e => { if (e.data && e.data.type === 'SKIP_WAITING') self.skipWaiting(); });
